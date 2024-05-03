c5f879913b7dcf338dbd02511d169082039870d4
Melih Cosgun <e12225987@student.tuwien.ac.at>
2024-05-03 03:54:10 +0200

    [Feature] Add Spline viewer components to home component
    - Implement two Spline viewer components in the home component
    - Style the Spline viewers to position them on top of each other with rounded corners and horizontal centering
    - Update the home component to load the Spline viewer script using CUSTOM_ELEMENTS_SCHEMA

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,14 +1,20 @@
-// src/app/home/home.component.ts
-import {Component} from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+// home.component.ts
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage]
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    const splineScript = document.createElement('script');
+    splineScript.setAttribute('type', 'module');
+    splineScript.setAttribute('src', 'https://unpkg.com/@splinetool/viewer@1.2.4/build/spline-viewer.js');
+    document.head.appendChild(splineScript);
+  }
 }
